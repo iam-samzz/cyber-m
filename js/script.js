44321e66da729e5147b847743eceae9bcba95754
Iftesam Parvin <iftesam123parvin@gmail.com>
2026-09-03 10:49:36 +0530

    Added Footer changes
    Footer changes

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* ==========================================
+   AUTOMATIC FOOTER YEAR
+========================================== */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const currentYear = document.getElementById("currentYear");
+
+    if (currentYear) {
+        currentYear.textContent = new Date().getFullYear();
+    }
+
+});
